f7d1dfe29dc65d7302dac1dfd24c0e299b042afc
MEYNERT Alison <ameyner2@login03.ecdf.ed.ac.uk>
2016-11-21 09:51:08 +0000

    Added protein-coding-only filter

--- src/perl/format_vep_extra_columns.pl
+++ src/perl/format_vep_extra_columns.pl
@@ -18,23 +18,30 @@ use strict;
 
 # Perl
 use IO::File;
+use IO::Zlib;
 use Sys::Hostname;
 use Getopt::Long;
 
 my $usage = qq{USAGE:
 $0 [--help]
-  --input    Input VEP file
-  --output   Output VEP file
+  --input               Input VEP file
+  --output              Output VEP file
+  --skip-non-genic      Skips lines where the only consequence is one of "intergenic_variant, downstream_gene_variant, upstream_gene_variant, intron_variant"
+  --protein-coding-only Only outputs lines where the feature biotype is "protein_coding"
 };
 
 my $help = 0;
 my $input;
 my $output;
+my $skip_non_genic = 0;
+my $protein_coding_only = 0;
 
 GetOptions(
     'help'     => \$help,
     'input=s'  => \$input,
     'output=s' => \$output,
+    'skip-non-genic' => \$skip_non_genic,
+    'protein-coding-only' => \$protein_coding_only
     ) or die $usage;
 
 if ($help || !$input || !$output)
@@ -43,11 +50,29 @@ if ($help || !$input || !$output)
     exit(0);
 }
 
-my $in_fh = new IO::File;
-$in_fh->open($input, "r") or die "Could not open $input\n$!";
+my $in_fh;
+my $out_fh;
+if ($input =~ /gz$/)
+{
+    $in_fh = new IO::Zlib;
+    $in_fh->open($input, "rb") or die "Could not open $input\n$!";
+}
+else
+{
+    $in_fh = new IO::File;
+    $in_fh->open($input, "r") or die "Could not open $input\n$!";
+}
 
-my $out_fh = new IO::File;
-$out_fh->open($output, "w") or die "Could not open $output\n$!";
+if ($output =~ /gz$/)
+{
+    $out_fh = new IO::Zlib;
+    $out_fh->open($output, "wb") or die "Could not open $output\n$!";
+}
+else
+{
+    $out_fh = new IO::File;
+    $out_fh->open($output, "w") or die "Could not open $output\n$!";
+}
 
 my $standard_cols;
 my @column_keys;
@@ -86,7 +111,11 @@ while (my $line = <$in_fh>)
     {
 	$last_idx = scalar(@tokens) - 1;
     }
-    printf $out_fh "%s", join("\t", @tokens[0..$last_idx]);
+
+    next if ($tokens[6] eq "intergenic_variant" || $tokens[6] eq "downstream_gene_variant" || $tokens[6] eq "upstream_gene_variant");
+    next if ($tokens[6] eq "intron_variant");
+
+    my $output_line = join("\t", @tokens[0..$last_idx]);
 
     my %info;
 
@@ -106,6 +135,7 @@ while (my $line = <$in_fh>)
 	}
     }
 
+    my $is_protein_coding = 0;
     for (my $i = 0; $i < scalar(@column_keys); $i++)
     {
 	if (exists($info{$column_keys[$i]}))
@@ -125,19 +155,31 @@ while (my $line = <$in_fh>)
 		    $o .= sprintf("$class:%d,", $counts{$class});
 		}
 		chop $o;
-		printf $out_fh "\t$o";
+		$output_line .= "\t$o";
+	    }
+	    elsif ($column_keys[$i] eq "BIOTYPE")
+	    {
+		if ($info{$column_keys[$i]} eq "protein_coding")
+		{
+		    $is_protein_coding = 1;
+		}
+		$output_line .= "\t" . $info{$column_keys[$i]};
 	    }
 	    else
 	    {
-		printf $out_fh "\t%s", $info{$column_keys[$i]};
+		$output_line .= "\t" . $info{$column_keys[$i]};
 	    }
 	}
 	else
 	{
-	    print $out_fh "\t.";
+	    $output_line .= "\t.";
 	}
     }
-    print $out_fh "\n";
+
+    if (!$protein_coding_only || ($protein_coding_only && $is_protein_coding))
+    {
+	print $out_fh "$output_line\n";
+    }
 }
 
 $in_fh->close();
